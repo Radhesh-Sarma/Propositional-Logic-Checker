5a93c781f9b94eb662e16e0ca900d8f0544dcce5
bhvdsi <54329549+bhvdsi@users.noreply.github.com>
2019-10-14 18:55:02 +0530

    Add files via upload

---
+++ day1.c
@@ -0,0 +1,34 @@
+//Team Nomenclature: A38
+//1. Radhesh Sarma (2017B4A70886H)
+//2. Harshit Gakhar (2018A7PS0182H)
+//3. Bhavyesh Desai (2018A7PS0164H)
+
+void push(char stack[], int max, int *stackPointer, char value)
+{
+	*stackPointer = *stackPointer + 1; //increase stackPointer by 1
+	stack[*stackPointer] = value; //add value to top of stack
+}
+
+void pop(char stack[], int max, int *stackPointer)
+{
+	*stackPointer = *stackPointer - 1; //decrease stackPointer by 1
+}
+
+int isEmpty(char stack[], int max, int stackPointer)
+{
+	if(stackPointer == -1)	//if stack is empty
+		return 1;			//return 1
+	return 0;				//else return 0
+}
+
+int isFull(char stack[], int max, int stackPointer)
+{
+	if(stackPointer == max-1)	//if stack is full
+		return 1;				//return 1
+	return 0;					//else return 0
+}
+
+char top(char stack[], int max, int stackPointer)
+{
+	return stack[stackPointer];	//return top element of stack
+}
