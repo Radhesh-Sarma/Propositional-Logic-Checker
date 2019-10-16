86a6bd6f143bfd41c01548322bdbca3e036d0d68
Harshit Gakhar <30308391+Harshit-Gakhar@users.noreply.github.com>
2019-10-16 14:07:54 +0530

    added team nomenclature

--- Day3_16Oct/modifiedDay1.c
+++ Day3_16Oct/modifiedDay1.c
@@ -1,6 +1,9 @@
 
 // contains functions for basic operations on stack when the dataype is a pointer.
-// author : Rashi Jain, 11/10/19
+//Team Nomenclature: A38
+//1. Radhesh Sarma (2017B4A70886H)
+//2. Harshit Gakhar (2018A7PS0182H)
+//3. Bhavyesh Desai (2018A7PS0164H)
 
 // stack : Array of pointers(to node).
 // max : integer variable denoting maximum size of array.
