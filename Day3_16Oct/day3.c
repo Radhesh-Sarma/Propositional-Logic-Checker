8b65a27bcbb67164fb4ae60d31ffb7194fe05f4f
Harshit Gakhar <30308391+Harshit-Gakhar@users.noreply.github.com>
2019-10-16 14:06:55 +0530

    added team nomenclature

--- Day3_16Oct/day3.c
+++ Day3_16Oct/day3.c
@@ -1,20 +1,18 @@
 // contains functions to construct a parse tree and print the infix expression.
-// author : Rashi Jain, 11/10/19
+//Team Nomenclature: A38
+//1. Radhesh Sarma (2017B4A70886H)
+//2. Harshit Gakhar (2018A7PS0182H)
+//3. Bhavyesh Desai (2018A7PS0164H)
 
-// prints the infix expression via inorder traversal.
-// root : Pointer to root of the binary tree. (dataype : node *)
-// node dataype is defined as
-// typedef struct node
-// {
-//		char ch;
-//   	struct node *left;
-//		struct node *right;
-// }node;
-// return : void
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #include "modifiedDay1.h"
+// prints the infix expression via inorder traversal.
+// root : Pointer to root of the binary tree. (dataype : node *)
+// return : void
+
 void inorder(node *root)
 {
     if(root->right!=NULL) printf("(");
