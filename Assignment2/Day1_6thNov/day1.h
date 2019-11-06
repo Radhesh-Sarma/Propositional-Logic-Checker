4a370264ee0db51ac9ccea9d0179ddd2f6a47c1f
Radhesh Sarma <50641461+Radhesh-Sarma@users.noreply.github.com>
2019-11-06 15:35:54 +0530

    Added Comments

--- Assignment2/Day1_6thNov/day1.h
+++ Assignment2/Day1_6thNov/day1.h
@@ -1,6 +1,11 @@
 // contains function to make any propositional logic formula implication-free. 
 // author : Rashi Jain, 30/10/19
 
+//Team Nomenclature: A38
+//1. Radhesh Sarma (2017B4A70886H)
+//2. Harshit Gakhar (2018A7PS0182H)
+//3. Bhavyesh Desai (2018A7PS0164H)
+
 // Removes all implications from the parse tree.   
 // root : Pointer to root of the parse tree(dataype : node *)
 // node dataype is defined as
