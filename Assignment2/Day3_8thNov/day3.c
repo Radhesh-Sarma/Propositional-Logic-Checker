989d7ecf34cd28a6aeaa04ebb4be8cdabdc9831b
Bhavyesh Desai <bhvdsi+github.bhvdsi@gmail.com>
2019-11-08 13:46:13 +0530

    fix bug in day3.c

--- Assignment2/Day3_8thNov/day3.c
+++ Assignment2/Day3_8thNov/day3.c
@@ -12,64 +12,46 @@
 #include "day2.h"
 #include "day3.h"
 
-node *cnf(node *root)
-{
-    char temp = root->ch;
-    
-	if(temp != '^' && temp != 'V' && temp != '>' && temp != '~')
-		return root; //Check if it is an Propositional Atom 
-
-    else if(temp == '^')
-    {
-         //if the expression is of the form P ^ Q , then return CNF(P) ^ CNF(Q)
-        cnf(root->left);
-        cnf(root->right);
-    }
-    else if(temp == 'V')
-    {
-        //if the expression is of the form P V Q , then return DISTR(CNF(P),CNF(Q))
-         
-         root = distr(cnf(root->left),cnf(root->right));
-    }
-
-
-    return root;
-
-}
-
 node *distr(node *n1, node *n2)
 {
-
-    char temp1 = n1->ch;
-    if(temp1 == '^')
-    {
-        // if n1 is of the form n11 ^ n12 return DISTR(n11,n2) ^ DISTR(n12,n2) 
-        struct node *pp = (struct node *)malloc(sizeof(struct node));
-        pp->ch = '^';
-        pp->left = distr(n1->left,n2);
-        pp->right = distr(n1->right,n2);
-        return pp;
-    }
-
-    char temp2 = n2->ch;
-
-    if(temp2 == '^')
-    {
-        // if n2 is of the form n21 ^ n22 return DISTR(n1,n21) ^ DISTR(n1,n22)
-        struct node *pp = (struct node *)malloc(sizeof(struct node));
-        pp->ch = '^';
-        pp->left = distr(n1,n2->left);
-        pp->right = distr(n1,n2->right);
-        return pp;
-    }
-
-    // Since there are no conjunctions return n1 V n2 
-    struct node *pp = (struct node *)malloc(sizeof(struct node));
-    pp->ch = 'V';
-     pp->left = n1;
-     pp->right = n2;
-
-     return pp;           
-
+	if(n1->ch == '^') //if n1 is of the form n11 ^ n12 return distr(n11,n2)^distr(n12,n2) 
+	{
+		struct node *pp = (struct node *)malloc(sizeof(struct node));
+		pp->ch = '^';
+		pp->left = distr(n1->left, n2);
+		pp->right = distr(n1->right, n2);
+		return pp;
+	}
+	else if(n2->ch == '^') //if n2 is of the form n21 ^ n22 return distr(n1,n21)^distr(n1,n22)
+	{
+		struct node *pp = (struct node *)malloc(sizeof(struct node));
+		pp->ch = '^';
+		pp->left = distr(n1, n2->left);
+		pp->right = distr(n1, n2->right);
+		return pp;
+	}
+	else //since there are no conjunctions, return n1 V n2 
+	{
+		struct node *pp = (struct node *)malloc(sizeof(struct node));
+		pp->ch = 'V';
+		pp->left = n1;
+		pp->right = n2;
+		return pp;
+	}
 }
 
+node *cnf(node *root)
+{
+	if(root->ch != '^' && root->ch != 'V' && root->ch != '>') //if subtree represents a literal, i.e., p or ~p
+		return root;
+	else if(root->ch == '^') //if the expression is of the form p^q , then return cnf(p)^cnf(q)
+	{
+		cnf(root->left);
+		cnf(root->right);
+	}
+	else //if the expression is of the form pVq , then return distr(cnf(p), cnf(q))
+	{
+		*root = *distr(cnf(root->left), cnf(root->right));
+	}
+	return root;
+}
